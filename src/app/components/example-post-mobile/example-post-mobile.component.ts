2589c01d2c6347716b4bbf068e7b6d26b65d3c43
wrobel2131 <wrobelek2131@wp.pl>
2024-02-04 11:41:20 +0100

    added media queries do landing page

---
+++ src/app/components/example-post-mobile/example-post-mobile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example-post-mobile',
+  standalone: true,
+  imports: [],
+  templateUrl: './example-post-mobile.component.svg',
+  styleUrl: './example-post-mobile.component.scss'
+})
+export class ExamplePostMobileComponent {
+
+}
